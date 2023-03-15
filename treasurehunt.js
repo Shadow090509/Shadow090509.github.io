d62c3d4808943f210627ecdd630b2ff2715105d3
Shadow090509 <jamoso090509@gmail.com>
2023-03-15 10:25:58 -0700

    changing the them for saint patrick's day and make sure the page works properly

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,50 +1,41 @@
-let yourName = localStorage.getItem("yourName");
-let costume = localStorage.getItem("costume");
-window.onload = function () {
-  alert("I hope you are ready for Hollows Eve...");
-  if (!yourName) {
-    yourName = prompt("What is your Name?");
-    localStorage.setItem("yourName", yourName);
-  }
-  if (!costume) {
-    costume = prompt("What kind of Monster are you this Halloween?");
-    localStorage.setItem("costume", costume);
-  }
-  alert(`Trick or Treat, ${yourName} the ${costume}. Good luck`);
-};
-let monster = Math.floor(Math.random() * 9);
-let candy = Math.floor(Math.random() * 9);
+var yourName = localStorage.getItem("yourName");
 
-if (monster === candy) {
+if (!yourName) {
+  yourName = prompt("What's yer name, laddy/lass?");
+  localStorage.setItem("yourName", yourName);
+}
+let costume = " Non-Leprechauns";
+let killer = Math.floor(Math.random() * 9);
+let gold = Math.floor(Math.random() * 9);
+
+if (killer === gold) {
   candy = Math.floor(Math.random() * 9);
 }
 let gameEnded = false;
-
 const treasure = (id) => {
   if (gameEnded) {
     return;
   }
-  const tombstone = document.getElementById(id);
-  if (tombstone.classList.contains("clicked")) {
-    return;
-  }
-  if (id === monster) {
-    tombstone.innerHTML =
-      '<img src="https://i.gifer.com/UVZ3.gif" height="175px" width="175px">';
-    alert(
-      `${costume}s must not like candy.. better luck next time ${yourName}...`
-    );
+  if (id === killer) {
+    document.getElementById(killer).innerHTML =
+      '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHEnxN_ukImFfJupIx_1ArdyqCWGi70w1wES9PRjG5QA&usqp=CAU&ec=48600112" height="175px" width="175px">';
+    setTimeout(() => {
+      alert(
+        `Ah, 'tis a pity that the${costume} didn't fancy the gold. Better luck next time, ${yourName}!`
+      );
+    }, 500);
     gameEnded = true;
-  } else if (id === candy) {
-    tombstone.innerHTML =
-      '<img src="https://i.pinimg.com/originals/bb/3e/40/bb3e4072995649e40e7a405b105c5420.gif" height="175px" width="175px">';
-    alert(
-      `${costume}s must be good at finding candy! Here is your candy, ${yourName}... I DARE YOU TO TRY IT AGAIN! `
-    );
+  } else if (id === gold) {
+    document.getElementById(gold).innerHTML =
+      '<img src="https://freesvg.org/img/Pot-Of-Gold.png" height="175px" width="175px">';
+    setTimeout(() => {
+      alert(
+        `Faith and begorrah! Ye ${costume}  be mighty good at findin' me pot of gold! Happy St. Patrick's Day, ${yourName}! But don't ye be gettin' too cocky now... I dare ye to try yer luck again! `
+      );
+    }, 500);
     gameEnded = true;
-  } else if (id !== candy && id !== monster) {
-    tombstone.innerHTML =
-      '<img src="https://miro.medium.com/max/996/1*o2llW65bX46N5UjmMegNGQ.gif" height="175px" width="175px">';
+  } else if (id !== gold && id !== killer) {
+    document.getElementById(id).innerHTML =
+      '<img src="https://freesvg.org/img/OneSeventeen-Pot-of-Gold.png" height="175px" width="175px">';
   }
-  tombstone.classList.add("clicked");
 };
