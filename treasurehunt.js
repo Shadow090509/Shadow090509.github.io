0e45d1c58e89e4a73d0f4794f0ebe0446606a0d1
Shadow090509 <jamoso090509@gmail.com>
2023-03-20 11:54:04 -0700

    first changes for easter

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,14 +1,14 @@
 var yourName = localStorage.getItem("yourName");
 
 if (!yourName) {
-  yourName = prompt("What's yer name, laddy/lass?");
+  yourName = prompt("What's your name?");
   localStorage.setItem("yourName", yourName);
 }
-let costume = " Non-Leprechauns";
-let killer = Math.floor(Math.random() * 9);
-let gold = Math.floor(Math.random() * 9);
+let costume = " Egg Hunter";
+let bunny = Math.floor(Math.random() * 9);
+let egg = Math.floor(Math.random() * 9);
 
-if (killer === gold) {
+if (bunny === egg) {
   candy = Math.floor(Math.random() * 9);
 }
 let gameEnded = false;
@@ -16,26 +16,26 @@ const treasure = (id) => {
   if (gameEnded) {
     return;
   }
-  if (id === killer) {
-    document.getElementById(killer).innerHTML =
-      '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHEnxN_ukImFfJupIx_1ArdyqCWGi70w1wES9PRjG5QA&usqp=CAU&ec=48600112" height="175px" width="175px">';
+  if (id === bunny) {
+    document.getElementById(bunny).innerHTML =
+      '<img src="https://live.staticflickr.com/7195/6881177214_525ed7c45e_c.jpg" height="175px" width="175px">';
     setTimeout(() => {
       alert(
-        `Ah, 'tis a pity that the${costume} didn't fancy the gold. Better luck next time, ${yourName}!`
+        `What a shame the easter bunny found the egg first!  Better luck next time, ${yourName}!`
       );
     }, 500);
     gameEnded = true;
-  } else if (id === gold) {
-    document.getElementById(gold).innerHTML =
-      '<img src="https://freesvg.org/img/Pot-Of-Gold.png" height="175px" width="175px">';
+  } else if (id === egg) {
+    document.getElementById(egg).innerHTML =
+      '<img src="https://www.publicdomainpictures.net/pictures/320000/nahled/pink-gold-easter-egg.png"height="175px" width="175px">';
     setTimeout(() => {
       alert(
-        `Faith and begorrah! Ye ${costume}  be mighty good at findin' me pot of gold! Happy St. Patrick's Day, ${yourName}! But don't ye be gettin' too cocky now... I dare ye to try yer luck again! `
+        `The ${costume} found the egg! Happy Easter ${yourName}! How many eggs can you find? `
       );
     }, 500);
     gameEnded = true;
-  } else if (id !== gold && id !== killer) {
+  } else if (id !== egg && id !== bunny) {
     document.getElementById(id).innerHTML =
-      '<img src="https://freesvg.org/img/OneSeventeen-Pot-of-Gold.png" height="175px" width="175px">';
+      '<img src="https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png" height="175px" width="175px">';
   }
 };
